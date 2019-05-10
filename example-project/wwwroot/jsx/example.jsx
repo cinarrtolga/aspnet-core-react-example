01f7b6713d5ced9e913095a22f1e6871e167998e
Tolga Çınar <tolgac@d-teknoloji.com.tr>
2019-05-10 11:19:09 +0300

    Example Project
    First commit

---
+++ example-project/wwwroot/jsx/example.jsx
@@ -0,0 +1,29 @@
+﻿class ExampleComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <ListComponent />
+                <FormComponent />
+            </div>
+        );
+    }
+}
+
+class ListComponent extends React.Component {
+    render() {
+        return (
+            <div>Options will list this area</div>
+        );
+    }
+}
+
+class FormComponent extends React.Component {
+    render() {
+        return (
+            <div>This area for the forms</div>
+        );
+    }
+}
+
+ReactDOM.render(<ExampleComponent />, document.getElementById('react-example'));
+
